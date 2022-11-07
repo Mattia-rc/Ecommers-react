6f0e841e5f672093b758936c6b5aa50e540d6378
carlflex <Ricardogugliermino@gmail.com>
2022-11-07 10:06:30 -0300

    primer commit

---
+++ src/componentes/ItemListContainer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function ItemListContainer() {
+    
+  return (
+    <ul>
+        <li>HOME</li>
+        <li>PRODUCTOS</li>
+        <li>CONTACTO</li>
+    </ul>
+  )
+}
---
+++ src/componentes/NavBar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import CartWidget from './CartWidget'
+
+export default function NavBar() {
+  return (
+    <nav>
+        <CartWidget/>
+        <h1>Es una nav</h1>
+    </nav>
+  )
+}
