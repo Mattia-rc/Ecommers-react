60ab74be584f285580952765d955deba66d1d41f
Mattia-rc <software@consultar.org>
2022-11-11 16:26:17 -0300

    agregando

--- src/componentes/ItemListContainer.jsx
+++ src/componentes/ItemListContainer.jsx
@@ -1,9 +1,39 @@
-  
+import { useState } from "react";
+const Productos = [
+
+  { id: 1, nombre: "teclado", img: "https://http2.mlstatic.com/D_NQ_NP_649496-MLA51811501299_102022-O.webp" },
+  { id: 2, nombre: "Mouse" }
+
+]
+
 const ItemListContainer = ({ greeting }) => {
-  return(
+
+  const [products, setProducts] = useState([]);
+
+  const productList = new Promise((resolve) => setTimeout(() => {
+    resolve(Productos);
+  }, 3000))
+
+  productList.then((data) => setProducts((data)))
+
+  return (
 
     <div>
-      <h2 className="texth2">{greeting}</h2>
+      {/*   <h2 className="texth2">{greeting}</h2> */}
+
+
+
+      <ul>{products.map((product) =>
+      <div>
+        <li>
+          {product.nombre}
+        </li>
+
+        <img src={product.img}/>
+        </div>
+      )}
+
+      </ul>
     </div>
 
   )
