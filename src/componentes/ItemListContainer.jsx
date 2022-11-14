2951e0964276f695a68f9f8d34cd10fa70f2ec37
Mattia-rc <software@consultar.org>
2022-11-14 14:15:24 -0300

    Agregar

--- src/componentes/ItemListContainer.jsx
+++ src/componentes/ItemListContainer.jsx
@@ -1,10 +1,12 @@
-  import { useState } from "react";
-  const Productos = [
+import { useState } from "react";
+const Productos = [
 
-    {id: 1,nombre: "teclado", img: "https://http2.mlstatic.com/D_NQ_NP_649496-MLA51811501299_102022-O.webp"},
-    {id: 2, nombre: "Mouse"}
+  { id: 1, nombre: "Teclado", stock: 4, img: "https://http2.mlstatic.com/D_NQ_NP_649496-MLA51811501299_102022-O.jpg" },
+  { id: 2, nombre: "Mouse", stock: 3, img: "https://medias.musimundo.com/medias/00181095-136169-136169-01-136169-01.jpg-size515?context=bWFzdGVyfGltYWdlc3w1OTQ4M3xpbWFnZS9qcGVnfGhlOS9oMDIvMTAzNzg4NzU3NjQ3NjYvMDAxODEwOTUtMTM2MTY5LTEzNjE2OV8wMS0xMzYxNjlfMDEuanBnX3NpemU1MTV8ZTdhMzk0ZTA2MDNhNWE0ZWMwMWNjNWJjMTVhZWVjNzUwMzZlMmMzMTQ4Mzg3MzQ0NmI0YjlkZjIyYjQwM2YzZA" },
+  { id: 3, nombre: "MousePad", stock: 5, img: "https://http2.mlstatic.com/D_NQ_NP_634717-MLA40721130270_022020-O.webp" },
+  { id: 4, nombre: "Monitor", stock: 10, img: "https://http2.mlstatic.com/D_NQ_NP_973781-MLA48131216539_112021-O.webp" }
 
-  ]
+]
 
 const ItemListContainer = ({ greeting }) => {
 
@@ -14,16 +16,39 @@ const ItemListContainer = ({ greeting }) => {
     resolve(Productos);
   }, 3000))
 
-    productList.then((data)=>setProducts((data)))
+  productList.then((data) => setProducts((data)))
 
-  return(
+  function Pantalla(){
+    
+  }
+  return (
 
     <div>
-    {/*   <h2 className="texth2">{greeting}</h2> */}
-      <ul>{products.map((product)=> <li>{product.nombre}</li>)}</ul>
-    </div>
+      {<h2 className="texth2">{greeting}</h2>}
+      {/*      <ul>{products.map((product)=> <li>{product.nombre}</li>)}</ul> 
+ */}
 
+      <div className="flex_productos">{products.map((product) =>
+
+      
+      <div className="img_edit"><img src={product.img}/>
+      
+      <h3>{product.nombre}</h3>
+      <p>el stock disponibles es de: {product.stock}</p>
+
+      <button onClick={Pantalla()}>Agregar Productos</button>
+      
+      </div>
+        
+    
+      
+
+      )}</div>
+    </div>
+    
   )
 }
+
+
 
 export default ItemListContainer;
