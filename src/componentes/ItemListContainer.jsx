07a2c4eae84b58d3b9d435584f089435a60de50d
Mattia Bagni <matutebagni@gmail.com>
2022-12-16 11:30:22 -0300

    Corrigiendo"

--- src/componentes/ItemListContainer.jsx
+++ src/componentes/ItemListContainer.jsx
@@ -3,46 +3,38 @@ import { useParams } from "react-router-dom";
 import { item } from "../mocks/item.mock"
 import { Link } from 'react-router-dom'
 import { CartContext } from "../context/cartContext";
-import {collection,doc,getDoc,getDocs,getFirestore} from "firebase/firestore"
+import {collection,doc,getDoc,getDocs,getFirestore, query,
+  where} from "firebase/firestore"
 const ItemListContainer = ({ greeting }) => {
   
   const { category } = useParams();
   const [products, setProducts] = useState([]);
   const [clicks, setClicks] = useState(0)
 
-  useEffect(()=>{
-
-    const db = getFirestore()
-   /*  const itemRef = doc(db, "item", "AP3JUadJ5OpbZ8LPZGyZ");
-    getDoc(itemRef).then((snapshot) =>{
-      if(snapshot.exists()){
-        setProducts([{id: "AP3JUadJ5OpbZ8LPZGyZ", ...snapshot.data()}]);
-      }
-    }) */
-    const itemCollection = collection(db, "item");
-    getDocs(itemCollection).then((snapshot)=>{
-      const products = snapshot.docs.map((doc)=>({id: doc.id, ...doc.data()}))
-      setProducts(products)
-    })
-  }, [])
+  useEffect(() => {
 
 
- /*  useEffect(() => {
-    new Promise((resolve) =>
-      setTimeout(() => {
-        resolve(item);
-      }, 2000)
-    ).then((data) => {
-      if (category) {
-        const categories = data.filter(
-          (product) => product.category === category
-        );
-        setProducts(categories);
-      } else {
-        setProducts(data);
-      }
-    });
-  }, [category]); */
+    if (!category) {
+      const db = getFirestore();
+
+      const itemsCollection = collection(db, "item");
+      getDocs(itemsCollection).then((result) =>
+        setProducts(result.docs.map((doc) => ({ id: doc.id, ...doc.data() })))
+      );
+    } else {
+      const db = getFirestore();
+
+      const q = query(
+        collection(db, "item"),
+        where("category", "==", category)
+      );
+      getDocs(q).then((result) =>
+        setProducts(result.docs.map((doc) => ({ id: doc.id, ...doc.data() })))
+      );
+    }
+  }, [category]);
+
+
 
   if (products.length === 0) {
     return <p>cargando....  </p>;
--- src/componentes/itemDetailContainer.jsx
+++ src/componentes/itemDetailContainer.jsx
@@ -3,27 +3,21 @@ import ItemDetail from "./ItemDetail";
 
 import { item, item as itemMock } from "../mocks/item.mock";
 import { useParams } from "react-router-dom";
+import { doc, getDoc, getFirestore } from "firebase/firestore";
 
 const ItemDetailContainer = () => {
   const [desc, setDesc] = useState({});
   const {id} = useParams();
 
   useEffect(() => {
-
-    new Promise((resolve) => setTimeout(() => resolve(itemMock[0]))).then(
-      (data) => {
-
-        if(desc){
-          const items = item.find(
-            (product)=> product.id === id
-          );
-          setDesc(items)
-        }else {
-          setDesc(data);
-        }
-      } 
-    );
-  }, []);
+    const db = getFirestore()
+      const itemRef = doc(db, "item", id);
+    getDoc(itemRef).then((snapshot) =>{
+      if(snapshot.exists()){
+        setDesc({id: snapshot.id, ...snapshot.data()});
+      }
+    }) 
+  }, [id]);
 
   
 
